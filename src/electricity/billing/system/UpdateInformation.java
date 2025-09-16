ff325c26f5067e62cc6df363eddb03759d4c1584
shivam9266 <shivam.patnaik02022004@gmail.com>
2025-09-16 23:07:59 +0530

    update

--- src/electricity/billing/system/UpdateInformation.java
+++ src/electricity/billing/system/UpdateInformation.java
@@ -8,6 +8,7 @@ import java.awt.event.*;
 public class UpdateInformation extends JFrame implements ActionListener{
     
     JTextField tfaddress, tfcity, tfstate, tfemail, tfphone;
+    JLabel name;
     JButton update, cancel;
     String meter;
     
@@ -26,7 +27,7 @@ public class UpdateInformation extends JFrame implements ActionListener{
         lblname.setBounds(30, 70, 100, 20);
         add(lblname);
         
-        JLabel name = new JLabel("");
+        name = new JLabel("");
         name.setBounds(230, 70, 200, 20);
         add(name);
         
@@ -119,7 +120,25 @@ public class UpdateInformation extends JFrame implements ActionListener{
     }
     
     public void actionPerformed(ActionEvent ae){
-        
+        if(ae.getSource() == update){
+            String address = tfaddress.getText();
+            String city = tfcity.getText();
+            String state = tfstate.getText();
+            String email = tfemail.getText();
+            String phone = tfphone.getText();
+            
+            try{
+                Conn c = new Conn();
+                c.s.executeUpdate("update customer set address = '"+address+"', city = '"+city+"', state = '"+state+"', email = '"+email+"', phone = '"+phone+"' where meter_no = '"+meter+"'");
+                
+                JOptionPane.showMessageDialog(null, "User Information updated successfully.");
+                setVisible(false);
+            } catch(Exception e){
+                e.printStackTrace();
+            }
+        } else {
+            setVisible(true);
+        }
     }
         
     
